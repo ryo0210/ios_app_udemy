c34c11fb431c4e751e57d0df070d651cefb93ead
ryo0210 <yone0217@icloud.com>
2020-04-19 12:44:23 +0900

    [Add]:When the user clicks the "Add" button of the alert, the task is displayed on the screen.

--- Todoey/Todoey/ViewController.swift
+++ Todoey/Todoey/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 // 名前変更ができない泣
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Find Mike", "Buy Eggs", "Destory Demogorgon"]
+    var itemArray = ["Find Mike", "Buy Eggs", "Destory Demogorgon"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,12 +31,56 @@ class TodoListViewController: UITableViewController {
 
     // MARK - TableView Delegate Methods
     
+    // どの行が選択されたかを検出するデリゲートメソッド(didSelectRowAt indexPath)をテーブルに追加
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(itemArray[indexPath.row])
+        
+        // tableView.cellForRowで特定のindexPathにあるcellへの参照を取得する。
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
         
         // cellを選択した時に、灰色になった後すぐ元の表示にもどる。
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    // MARK - Adds new items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        //addButtonPressed内で使えるローカル変数を作成する。
+        var textField = UITextField()
+        
+        // アラートの構成。
+        let alert = UIAlertController(title: "新しいタスクを追加", message: "", preferredStyle: .alert)
+        // アラートの選択ボタン
+        let action = UIAlertAction(title: "追加", style: .default) { (action) in
+            // ユーザーがUIAlertの"追加"ボタンをクリックした後の処理。
+            
+            // textField.textが空なら"新しいタスク"という名前で追加する。
+            // itemArray.append(textField.text ?? "新しいタスク")
+            
+            //クロージャの中にいるので、itemArrayが存在する場所
+            //(現在の場所でコンパイラに明示的に指示するために「self」を指定する必要がある。
+            self.itemArray.append(textField.text!)
+            
+            // tableViewの行とセクションが再読み込みされる。
+            self.tableView.reloadData()
+        }
+        // このクロージャーはTextFieldがalertに追加された後にのみトリガーされる。
+        alert.addTextField { (aletTextField) in
+            aletTextField.placeholder = "新しいタスクを作る。"
+            // aletTextFieldはalert.addTextFieldのクロージャーの内部でしか使えないので、
+            // addButtonPressed全体で使えるように拡張する。
+            textField = aletTextField
+        }
+        
+        alert.addAction(action)
+        
+        // アラートを表示する。第一引数は表示するアラート。
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
 // tableViewのAccessoryで、cellを選択したとき別の場所に移動させるか、詳細を表示するかを選択できる。
