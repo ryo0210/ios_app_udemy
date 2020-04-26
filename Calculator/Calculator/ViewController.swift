f41acc6fd98aa9a00730cc5693f692258280957c
ryo0210 <yone0217@icloud.com>
2020-04-26 23:36:29 +0900

    Init project.

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by ryo on 2020/04/26.
+//  Copyright © 2020 ryo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var displayLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func calButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+    }
+    
+}
+
