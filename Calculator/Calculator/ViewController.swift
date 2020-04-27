bf96e6a4d1f7d24d1517b93d5de2b0a6e6b1c6ea
ryo0210 <yone0217@icloud.com>
2020-04-27 23:46:54 +0900

    [Add]:Enabled to input numbers.

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -12,12 +12,22 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var displayLabel: UILabel!
     
-
+    private var isFinishedTypeNumber: Bool = true
+    
 
     @IBAction func calButtonPressed(_ sender: UIButton) {
+        
     }
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
+        if let numValue = sender.currentTitle {
+            if isFinishedTypeNumber {
+                displayLabel.text = numValue
+                isFinishedTypeNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
     }
     
 }
