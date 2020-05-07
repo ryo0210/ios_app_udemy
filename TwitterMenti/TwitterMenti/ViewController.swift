7df3bf71e7288adec40ec5ec492c4b5a55c81f1f
ryo0210 <yone0217@icloud.com>
2020-05-07 23:39:21 +0900

    [Add]:I made the xcode project the same as the cloned state.

--- TwitterMenti/TwitterMenti/ViewController.swift
+++ TwitterMenti/TwitterMenti/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sentimentLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func predictPressed(_ sender: UIButton) {
+    }
+    
 }
 
